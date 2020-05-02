c4f5fb5cb7b03e28c711a4d5d8e85951e9c11711
Emre Kökçü <emre.kokcu@dip.com.tr>
2020-05-02 19:23:30 +0300

    update secret

--- src/main/java/com/kokcuemre/spring/security/jwt/constant/SecurityConstants.java
+++ src/main/java/com/kokcuemre/spring/security/jwt/constant/SecurityConstants.java
@@ -1,7 +1,7 @@
 package com.kokcuemre.spring.security.jwt.constant;
 
 public class SecurityConstants {
-	public static final String SECRET = "Emakina";
+	public static final String SECRET = "EmreKOKCU";
 	public static final long EXPIRATION_TIME = 300_000; // 5 minute
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
